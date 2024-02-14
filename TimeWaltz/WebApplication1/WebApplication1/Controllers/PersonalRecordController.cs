b6024eeb84cec6003819569fe9ea721b43b04188
Michelle <jiarongwu0130m@gmail.com>
2024-02-14 23:54:04 +0800

    新增打卡頁面

---
+++ TimeWaltz/WebApplication1/WebApplication1/Controllers/PersonalRecordController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonalRecordController : Controller
+    {
+        public IActionResult Clock()
+        {
+            return View();
+        }
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+        public IActionResult Leave()
+        {
+            return View();
+        }
+        public IActionResult Overtime()
+        {
+            return View();
+        }
+        public IActionResult ShiftSchedule()
+        {
+            return View();
+        }
+        public IActionResult Approval()
+        {
+            return View();
+        }
+        public IActionResult AgentEmployeeSetting()
+        {
+            return View();
+        }
+    }
+}
