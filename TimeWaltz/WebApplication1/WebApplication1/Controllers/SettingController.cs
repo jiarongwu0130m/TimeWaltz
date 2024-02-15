2e38728803449574ec0c2c4ab89aaf6d86876745
joli15742 <joli@richway.tw>
2024-02-15 15:10:05 +0800

    開會前暫存紀錄，增加帳號CRUD頁面，加密加鹽作法

---
+++ TimeWaltz/WebApplication1/WebApplication1/Controllers/SettingController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Cryptography;
+using System.Text;
+using WebApplication1.Models;
+using WebApplication1.Models.Entity;
+
+namespace WebApplication1.Controllers
+{
+    public class SettingController : Controller
+    {
+        private readonly TimeWaltzContext _db;
+
+        public SettingController(TimeWaltzContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// 雜湊SHA256
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string Get_SHA256_Hash(string value)
+        {
+            using var hash = SHA256.Create(); 
+            return hash.ComputeHash(Encoding.UTF8.GetBytes(value)).ToString();
+        }
+        /// <summary>
+        /// 隨機鹽，長度12碼
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateSalt()
+        {
+            char[] chars = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
+                            "abcdefghijklmnopqrstuvwxyz" +
+                    "1234567890"+
+                    "!@#$%^&*()_+").ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            var random = new Random();
+            for (int i = 0; i < 13; i++)
+            {
+                char aChar = chars[random.Next(chars.Length)];
+                sb.Append(aChar);
+            }
+            return sb.ToString();
+        }
+
+        #region 帳號設定 
+        /// <summary>
+        /// 帳號查詢select
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult AccountS()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 帳號空白新增頁
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Account()
+        {
+            ViewBag.GenderEnum = _db.Departments.Select(se => new SelectListItem
+            {
+                Text = se.DepartmentName.ToString(),
+                Value = se.DepartmentId.ToString()
+            });
+
+            return View();
+        }
+
+        /// <summary>
+        /// 帳號新增
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Account(AccountViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            //密碼加密加鹽
+            string Salts= GenerateSalt();
+            model.Password = Get_SHA256_Hash(model.Password+ Salts);
+
+
+            _db.Users.Add(new User
+            {
+                Account = model.Account,
+                Password = model.Password,
+                Stop = model.Stop,
+                Salt = Salts,
+                DepartmentId = Convert.ToInt32(model.DepartmentName),
+                PasswordDate = DateTime.Now
+            }) ;
+            _db.SaveChanges();
+
+            //未定義
+            return RedirectToAction("Setting", "AccountS");
+        }
+
+
+        /// <summary>
+        /// 帳號修改update
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Account(string id, AccountViewModel model)
+        {
+
+            //_db.Users.Add(new User
+            //{
+            //    Account = model.Account,
+            //    Password = model.Password,
+            //    Stop = model.Stop,
+            //    Salt = Salts,
+            //    DepartmentId = Convert.ToInt32(model.DepartmentName),
+            //    PasswordDate = DateTime.Now
+            //});
+            _db.SaveChanges();
+            return View();
+        }
+        /// <summary>
+        /// 帳號修改 依照ID給予資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Account(string id)
+        {
+            AccountViewModel data;
+
+
+            return View(data);
+        }
+
+        #endregion
+
+        #region 權限設定 
+        /// <summary>
+        /// ●權限設定
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Access()
+        {
+            return View();
+        }
+
+        #endregion
+        #region 角色設定 
+        /// <summary>
+        /// ●角色設定
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Role()
+        {
+            return View();
+        }
+
+        #endregion
+        #region 公佈欄 
+        /// <summary>
+        /// ●公佈欄
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult BillBoard()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
---
+++ TimeWaltz/WebApplication1/WebApplication1/Models/AccountViewModel.cs
@@ -0,0 +1,20 @@
+﻿using WebApplication1.Models.Entity;
+
+namespace WebApplication1.Models
+{
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string DepartmentName { get; set; } = null!;
+
+        public string Account { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public bool Stop { get; set; }
+
+    }
+}
