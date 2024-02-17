093c8c4b46de7fb47c56877f777e2e1086d031a1
joli15742 <joli@richway.tw>
2024-02-17 14:45:22 +0800

    update 增加欄位提示

--- TimeWaltz/WebApplication1/WebApplication1/Models/AccountViewModel.cs
+++ TimeWaltz/WebApplication1/WebApplication1/Models/AccountViewModel.cs
@@ -1,17 +1,19 @@
-﻿using WebApplication1.Models.Entity;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using WebApplication1.Models.Entity;
 
 namespace WebApplication1.Models
 {
     public class AccountViewModel
     {
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "請填寫暱稱")]
         public string Name { get; set; } = null!;
 
         public string DepartmentName { get; set; } = null!;
-
+        [Required(ErrorMessage = "請輸入帳號")]
         public string Account { get; set; } = null!;
-
+        [Required(ErrorMessage = "請輸入密碼")]
         public string Password { get; set; } = null!;
 
         public bool Stop { get; set; }
