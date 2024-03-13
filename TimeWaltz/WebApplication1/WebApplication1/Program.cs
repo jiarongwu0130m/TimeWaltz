649c65eadfc8f5ea893cff53ef93a936447f410d
Andy YC Wang <reds1210@gmail.com>
2024-03-13 13:20:22 +0800

    add area

--- TimeWaltz/WebApplication1/WebApplication1/Program.cs
+++ TimeWaltz/WebApplication1/WebApplication1/Program.cs
@@ -51,6 +51,9 @@ namespace WebApplication1
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
