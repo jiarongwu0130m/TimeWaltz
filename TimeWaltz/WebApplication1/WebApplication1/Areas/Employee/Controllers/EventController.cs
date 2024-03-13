c3eb9b130ced7074e652753171844da5dbec6536
reds1210 <reds1210@gmail.com>
2024-03-13 22:03:20 +0800

    2024/03/13

---
+++ TimeWaltz/WebApplication1/WebApplication1/Areas/Employee/Controllers/EventController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class EventController : Controller
+    {
+        /// <summary>
+        /// 打卡
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Clock()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 請假申請
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult LeaveRequest()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 加班申請
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OverTimeRequest()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 補打卡申請
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult CompRequest()
+        {
+            return View();
+        }
+    }
+}
