4b97caec2c70e4e289a874a2a99d9759872b4ba2
Michelle <jiarongwu0130m@gmail.com>
2024-03-16 20:35:54 +0800

    公佈欄頁面：加入CKEditor套件

---
+++ TimeWaltz/WebApplication1/WebApplication1/Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult BillBoard()
+        {
+            return View();
+        }
+    }
+}
