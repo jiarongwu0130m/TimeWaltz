2dcb67d8ab26d6b5aa4fe113d0f8a976f7eca7f2
joli15742 <joli@richway.tw>
2024-03-18 13:52:07 +0800

    add login功能製作完成

---
+++ TimeWaltz/WebApplication1/WebApplication1/Models/Account/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models.Account
+{
+    public class LoginViewModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+}
