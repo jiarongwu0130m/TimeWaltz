ce6e37ecac48576920fd4be674924275b46bd8df
Chloe <cherrylin30@gmail.com>
2024-03-20 14:53:59 +0800

    開始修請假的程式

--- TimeWaltz/WebApplication1/WebApplication1/Controllers/Api/LeaveApiController.cs
+++ TimeWaltz/WebApplication1/WebApplication1/Controllers/Api/LeaveApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Repository.Models;
 using WebApplication1.Helpers;
 using WebApplication1.Models.BasicSettingViewModels;
 using WebApplication1.Models.PersonalRecordViewModels;
@@ -10,17 +11,22 @@ namespace WebApplication1.Controllers.Api
     [ApiController]
     public class LeaveApiController : ControllerBase
     {
+        private readonly TimeWaltzContext _db;
         private readonly LeaveService _leaveService;
         private readonly AgentEmployeeService _agentEmployeeService;
         private readonly RequestStatusService _requestStatusService;
         private readonly ApprovalService _approvalService;
+        private readonly IWebHostEnvironment _env;
 
-        public LeaveApiController(LeaveService leaveService, AgentEmployeeService agentEmployeeService, RequestStatusService requestStatusService, ApprovalService approvalService)
+
+        public LeaveApiController(TimeWaltzContext timeWaltzContext , LeaveService leaveService, AgentEmployeeService agentEmployeeService, RequestStatusService requestStatusService, ApprovalService approvalService, IWebHostEnvironment webHostEnvironment)
         {
+            this._db = timeWaltzContext;
             _leaveService = leaveService;
             _agentEmployeeService = agentEmployeeService;
             this._requestStatusService = requestStatusService;
             this._approvalService = approvalService;
+            this._env = webHostEnvironment;
         }
         /// <summary>
         /// 請假申請單畫面，取下拉式選單資料
@@ -64,15 +70,32 @@ namespace WebApplication1.Controllers.Api
             try
             {
                 //在處理(多塞)從使用者那裡接收不到的資料時要再建一個新的更大的更多欄位的model才比較安全(正確)
-                var fileModel = _leaveService.GetRelativeFileRoute(model);
-                var approvalModel = _leaveService.GetApprovalEmp(fileModel);
-                var leaveModel = _leaveService.AddLeaveTime2(approvalModel);
-                var entity = ViewModelHelper.ToEntity(leaveModel);
-                _leaveService.CreateLeaveRequest(entity);
-                //TODO: 新增狀態表
-                _requestStatusService.NewRequestStatus(entity);
-                //TODO: 新增簽核表
-                _approvalService.NewApproval(entity);
+                
+                //var relativePath = "";
+                //if (model.FileRoute != null)
+                //{
+                //    var dir = $"{_env.WebRootPath}";
+                //    relativePath = $"/pic/{Guid.NewGuid()}_{model.FileRoute.FileName}";
+
+
+                //    using (var fs = new FileStream(dir + relativePath, FileMode.Create))
+                //    {
+                //        model.FileRoute.CopyTo(fs);
+                //    }
+
+                //    model.RelativeFileRoute = relativePath;
+
+                    
+                //}
+
+                //var approvalModel = _leaveService.GetApprovalEmp(fileModel);
+                //var leaveModel = _leaveService.AddLeaveTime2(approvalModel);
+                //var entity = ViewModelHelper.ToEntity(leaveModel);
+                //_leaveService.CreateLeaveRequest(entity);
+                ////TODO: 新增狀態表
+                //_requestStatusService.NewRequestStatus(entity);
+                ////TODO: 新增簽核表
+                //_approvalService.NewApproval(entity);
                 return Ok(new { status = true });
 
             }
