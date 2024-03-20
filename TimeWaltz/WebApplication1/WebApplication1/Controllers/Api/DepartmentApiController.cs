8de076d172c46f4165ccc784dbf2b1a3a4e8e726
Chloe <cherrylin30@gmail.com>
2024-03-20 21:53:52 +0800

    將部門改成Vue做到一半

---
+++ TimeWaltz/WebApplication1/WebApplication1/Controllers/Api/DepartmentApiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Models;
+using WebApplication1.Filters;
+using WebApplication1.Helpers;
+using WebApplication1.Models.BasicSettingViewModels;
+
+namespace WebApplication1.Controllers.Api
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class DepartmentApiController : ControllerBase
+    {
+        private readonly TimeWaltzContext _db;
+
+        public DepartmentApiController(TimeWaltzContext timeWaltzContext)
+        {
+            _db = timeWaltzContext;
+        }
+        [HttpGet]
+        public ActionResult<DepartmentCreateDto> GetDropDownList()
+        {
+            try
+            {
+                var empList = _db.Employees.ToList();
+                var model = new DepartmentCreateDto
+                {
+                    EmployeeNameSelectList = DropDownHelper
+                           .GetEmployeeNameDropDownList(empList),
+                };
+                return model;
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { status = false });
+            }
+        }
+    }
+}
