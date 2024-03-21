7ab0c725af285fd8f1c30484693a06a345639b3d
Andy YC Wang <reds1210@gmail.com>
2024-03-22 03:01:43 +0800

    重寫一次

---
+++ TimeWaltz/WebApplication1/WebApplication1/Areas/Employee/Controllers/Api/ShiftApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+using WebApplication1.Helpers;
+using WebApplication1.Models.BasicSettingViewModels;
+using WebApplication1.Services;
+
+namespace WebApplication1.Areas.Employee.Controllers.Api
+{
+    [Route("mobile/api/Shift/[action]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "EmployeeAuthScheme")]
+    public class ShiftApiController : ControllerBase
+    {
+        private readonly TimeWaltzContext _timeWaltzDb;
+
+        public ShiftApiController(TimeWaltzContext timeWaltzDb)
+        {
+            _timeWaltzDb = timeWaltzDb;
+        }
+
+        [HttpGet]
+        public IActionResult Schedule()
+        {
+            var shiftsResult = _timeWaltzDb.Shifts.AsNoTracking()
+                .Include(x => x.ShiftSchedule)
+                .Where(x => x.EmployeesId == User.GetId())
+                .Select(x=> new
+                {
+                    Id = x.Id,
+                    Start = x.ShiftsDate.Date + x.ShiftSchedule.StartTime.TimeOfDay,
+                    End = x.ShiftsDate.Date + x.ShiftSchedule.EndTime.TimeOfDay,
+                    AllDay = false,
+                    Title = x.ShiftSchedule.ShiftsName
+                });
+
+            return Ok(shiftsResult);
+
+        }
+    }
+}
